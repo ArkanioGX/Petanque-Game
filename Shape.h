c062a21e8772595aa70082d02f15e08c3c7f0f3f
Clarence Mollet <clarencemollet1@gmail.com>
2025-03-13 10:58:42 +0100

    [add] Angular And Linear velocity

--- Shape.h
+++ Shape.h
@@ -1,21 +1,32 @@
 #pragma once
 class Shape {
 public:
+	Shape() = default;
+	~Shape() = default;
+
 	enum class ShapeType
 	{
 		SHAPE_SPHERE,
 	};
 
+	virtual Mat3 InertiaTensor() const = 0;
+
 	virtual ShapeType GetType() const = 0;
+	virtual Vec3 GetCenterOfMass() const { return centerOfMass; }
+protected:
+	Vec3 centerOfMass;
 };
 
 class ShapeSphere : public Shape {
 public:
 	ShapeSphere(float radiusP) : radius(radiusP)
 	{
+		centerOfMass.Zero();
 	}
-
 	ShapeType GetType() const override { return ShapeType::SHAPE_SPHERE; }
 	float radius;
+
+	Mat3 InertiaTensor() const override;
+
 };
 
--- code/Scene.cpp
+++ code/Scene.cpp
@@ -3,6 +3,7 @@
 //
 #include "Scene.h"
 #include "../Shape.h"
+#include "../Intersections.h"
 
 
 /*
@@ -45,13 +46,32 @@ Scene::Initialize
 ====================================================
 */
 void Scene::Initialize() {
-	Body body;
-	body.position = Vec3( 0, 0, 0 );
-	body.orientation = Quat( 0, 0, 0, 1 );
-	body.shape = new ShapeSphere( 1.0f );
-	bodies.push_back( body );
-
-	// TODO: Add code
+	Body fast;
+	fast.position = Vec3(-3, 0, 3);
+	fast.orientation = Quat(0, 0, 0, 1);
+	fast.shape = new ShapeSphere(1.0f);
+	fast.inverseMass = 1.0f;
+	fast.elasticity = 0.5f;
+	fast.friction = 0.5f;
+	fast.linearVelocity = Vec3(5, 1, 1);
+	bodies.push_back(fast);
+	Body immobile;
+	immobile.position = Vec3(0, 0, 3);
+	immobile.orientation = Quat(0, 0, 0, 1);
+	immobile.shape = new ShapeSphere(1.0f);
+	immobile.inverseMass = 1.0f;
+	immobile.elasticity = 0.5f;
+	immobile.friction = 0.5f;
+	immobile.linearVelocity = Vec3(0, 0, 0);
+	bodies.push_back(immobile);
+	Body earth;
+	earth.position = Vec3(0, 0, -1000);
+	earth.orientation = Quat(0, 0, 0, 1);
+	earth.shape = new ShapeSphere(1000.0f);
+	earth.inverseMass = 0.0f;
+	earth.elasticity = 0.99f;
+	earth.friction = 0.5f;
+	bodies.push_back(earth);
 }
 
 /*
@@ -59,6 +79,69 @@ void Scene::Initialize() {
 Scene::Update
 ====================================================
 */
-void Scene::Update( const float dt_sec ) {
-	// TODO: Add code
+void Scene::Update(const float dt_sec)
+{
+	// Gravity
+	for (int i = 0; i < bodies.size(); ++i)
+	{
+		Body& body = bodies[i];
+		float mass = 1.0f / body.inverseMass;
+		// Gravity needs to be an impulse I
+		// I == dp, so F == dp/dt <=> dp = F * dt
+		// <=> I = F * dt <=> I = m * g * dt
+		Vec3 impulseGravity = Vec3(0, 0, -10) * mass * dt_sec;
+		body.ApplyImpulseLinear(impulseGravity);
+	}
+	// Collision checks
+	int numContacts = 0;
+	const int maxContacts = bodies.size() * bodies.size();
+	Contact* contacts = (Contact*)alloca(sizeof(Contact) * maxContacts);
+	for (int i = 0; i < bodies.size(); ++i)
+	{
+		for (int j = i + 1; j < bodies.size(); ++j)
+		{
+			Body& bodyA = bodies[i];
+			Body& bodyB = bodies[j];
+			if (bodyA.inverseMass == 0.0f && bodyB.inverseMass == 0.0f)
+				continue;
+			Contact contact;
+			if (Intersections::Intersect(bodyA, bodyB, dt_sec, contact))
+			{
+				contacts[numContacts] = contact;
+				++numContacts;
+			}
+		}
+	}
+	// Sort times of impact
+	if (numContacts > 1) {
+		qsort(contacts, numContacts, sizeof(Contact),
+			Contact::CompareContact);
+	}
+	// Contact resolve in order
+	float accumulatedTime = 0.0f;
+	for (int i = 0; i < numContacts; ++i)
+	{
+		Contact& contact = contacts[i];
+		const float dt = contact.timeOfImpact - accumulatedTime;
+		Body* bodyA = contact.a;
+		Body* bodyB = contact.b;
+		// Skip body par with infinite mass
+		if (bodyA->inverseMass == 0.0f && bodyB->inverseMass == 0.0f)
+			continue;
+		// Position update
+		for (int j = 0; j < bodies.size(); ++j) {
+			bodies[j].Update(dt);
+		}
+		Contact::ResolveContact(contact);
+		accumulatedTime += dt;
+	}
+	// Other physics behavirous, outside collisions.
+	// Update the positions for the rest of this frame's time.
+	const float timeRemaining = dt_sec - accumulatedTime;
+	if (timeRemaining > 0.0f)
+	{
+		for (int i = 0; i < bodies.size(); ++i) {
+			bodies[i].Update(timeRemaining);
+		}
+	}
 }
